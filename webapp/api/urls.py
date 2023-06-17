269a4d4302dffe39720825573916e0ed99372fbf
voidCaloneian <106653303+voidCaloneian@users.noreply.github.com>
2023-06-17 19:16:12 +0500

    Made more prettier

--- webapp/api/urls.py
+++ webapp/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+
 from .views import HallViewSet, HallPriceView, BookingCreateView, CheckBookingsByDayView
+
 from rest_framework.routers import DefaultRouter
+
 
 router = DefaultRouter()
 router.register('hall', HallViewSet, basename='hall')
