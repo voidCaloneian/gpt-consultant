ee7545bcc55c7d13c78c45ff7f39e336b77b73fe
voidCaloneian <void4function@gmail.com>
2023-06-20 07:19:17 +0500

    readability improvements

--- webapp/api/urls.py
+++ webapp/api/urls.py
@@ -1,13 +1,6 @@
 from django.urls import path
 
-from .views import (
-    HallDetailView, 
-    HallPriceView, 
-    CheckBookingsByDayView, 
-    HashBookingView,
-    CheckoutView,
-    HallDataView
-)
+from .views import *
 
 
 urlpatterns = [
